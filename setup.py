44b8a3e2b37d19f80cd44312db9a65b7ed79f5da
kalyands996 <vknarasimharao@gmail.com>
2025-07-17 18:57:05 +0530

    Project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kalyan Narasimha",
+    author_email="vknarasimharao@gmail.com",
+    packages=find_packages()
+)
