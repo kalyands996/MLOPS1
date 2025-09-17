948cad365e52c5ed6163bed8cd693a728c8987eb
kalyands996 <vknarasimharao@gmail.com>
2025-09-17 17:17:51 +0530

    project demo notebook added

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import os
 from logging.handlers import RotatingFileHandler
-from from_root import from_root
+import from_root
 from datetime import datetime
 
 # Constants for log configuration
